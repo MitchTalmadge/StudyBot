2c40ea651b53f7896c244a107f88d78df056a39c
Mitch Talmadge <mitch@mitchtalmadge.com>
2020-05-15 17:55:17 -0600

    Implement role assignment queue.

--- src/services/discord/role-assignment.ts
+++ src/services/discord/role-assignment.ts
@@ -4,6 +4,9 @@ import { CourseImplementDiscordService } from "./implement/course";
 import { GuildContext } from "src/guild-context";
 
 export class RoleAssignmentDiscordService {
+
+  private static roleAssignmentQueues: { [guildId: string]: Promise<void> } = {};
+
   /**
    * Queues course role additions for a member.
    * @param guildContext The guild context.
@@ -11,12 +14,19 @@ export class RoleAssignmentDiscordService {
    * @param courses The courses to add as roles.
    */
   public static queueCourseRolesAddition(guildContext: GuildContext, discordMember: Discord.GuildMember, courses: Course[]): void {
-    this.assignCourseRoles(guildContext, discordMember, courses, [])
-      .then(() => {
-        console.log("Roles assigned.");
-      })
-      .catch(err => {
-        console.error(`Failed to assign roles to member ${discordMember.id}:`, err);
+    if (!this.roleAssignmentQueues[guildContext.guild.id]) {
+      this.roleAssignmentQueues[guildContext.guild.id] = Promise.resolve();
+    }
+
+    this.roleAssignmentQueues[guildContext.guild.id] =
+      this.roleAssignmentQueues[guildContext.guild.id].finally(() => {
+        return this.assignCourseRoles(guildContext, discordMember, courses, [])
+          .then(() => {
+            console.log("Roles assigned.");
+          })
+          .catch(err => {
+            console.error(`Failed to assign roles to member ${discordMember.id}:`, err);
+          });
       });
   }
 
@@ -27,16 +37,23 @@ export class RoleAssignmentDiscordService {
    * @param courses The courses to remove as roles.
    */
   public static queueCourseRolesRemoval(guildContext: GuildContext, discordMember: Discord.GuildMember, courses: Course[]): void {
-    this.assignCourseRoles(guildContext, discordMember, [], courses)
-      .then(() => {
-        console.log("Roles removed.");
-      })
-      .catch(err => {
-        console.error(`Failed to remove roles from member ${discordMember.id}:`, err);
+    if (!this.roleAssignmentQueues[guildContext.guild.id]) {
+      this.roleAssignmentQueues[guildContext.guild.id] = Promise.resolve();
+    }
+
+    this.roleAssignmentQueues[guildContext.guild.id] =
+      this.roleAssignmentQueues[guildContext.guild.id].finally(() => {
+        return this.assignCourseRoles(guildContext, discordMember, [], courses)
+          .then(() => {
+            console.log("Roles removed.");
+          })
+          .catch(err => {
+            console.error(`Failed to remove roles from member ${discordMember.id}:`, err);
+          });
       });
   }
 
-  private static async assignCourseRoles(guildContext: GuildContext, discordMember: Discord.GuildMember, coursesToAdd: Course[], coursesToRemove: Course[]): Promise<void> {    
+  private static async assignCourseRoles(guildContext: GuildContext, discordMember: Discord.GuildMember, coursesToAdd: Course[], coursesToRemove: Course[]): Promise<void> {
     // Addition
     let rolesToAdd: string[] = [];
     for (let course of coursesToAdd) {
@@ -50,14 +67,14 @@ export class RoleAssignmentDiscordService {
     let rolesToRemove: string[] = [];
     for (let course of coursesToRemove) {
       const courseImplement = await CourseImplementDiscordService.getCourseImplementIfExists(guildContext, course);
-      if(courseImplement)
+      if (courseImplement)
         rolesToRemove.push(courseImplement.mainRoleId);
     }
     if (rolesToRemove.length > 0) {
       discordMember = await discordMember.roles.remove(rolesToRemove, "StudyBot automatic role removal");
 
       // Check if nobody is in the course anymore.
-      for(let course of coursesToRemove) {
+      for (let course of coursesToRemove) {
         await CourseImplementDiscordService.deleteCourseImplementIfEmpty(guildContext, course);
       }
     }
