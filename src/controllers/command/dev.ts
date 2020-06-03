3cfc46c295ed5fa0965386d1a6a8e13608378ed7
Mitch Talmadge <mitch@mitchtalmadge.com>
2020-06-03 00:14:24 -0600

    Add dev wipechannel command.

--- src/controllers/command/dev.ts
+++ src/controllers/command/dev.ts
@@ -4,28 +4,37 @@ import { DiscordUtils } from "utils/discord";
 
 export class DevCommandController extends CommandController {
   public onMessageReceived(message: Discord.Message | Discord.PartialMessage): void {
-    if(process.env.NODE_ENV !== "development") 
+    if (process.env.NODE_ENV !== "development")
       return;
 
-    if(!message.content.startsWith("!dev"))
+    if (!message.content.startsWith("!dev"))
       return;
-      
+
     const commandTokens = message.content.toLowerCase().split(/\s+/);
-    if(commandTokens.length == 1) {
+    if (commandTokens.length == 1) {
       message.reply("No command parameters were supplied.");
       return;
     }
 
-    if(commandTokens[1] === "reset") {
-      message.reply("reset initiated...");
-      this.resetGuild() 
-        .then(() => {
-          message.reply("reset complete!");
-        })
-        .catch(err => {
-          message.reply("something went wrong during reset. Check the logs.");
-          this.guildContext.guildError("Could not reset guild", err);
-        });
+    switch (commandTokens[1]) {
+      case "reset":
+        message.reply("reset initiated...");
+        this.resetGuild()
+          .then(() => {
+            message.reply("reset complete!");
+          })
+          .catch(err => {
+            message.reply("something went wrong during reset. Check the logs.");
+            this.guildContext.guildError("Could not reset guild:", err);
+          });
+      case "wipechannel":
+        if (message.channel.type === "text") {
+          this.wipeTextChannel(message.channel)
+            .catch(err => {
+              message.reply("something went wrong while wiping the channel. Check the logs.");
+              this,this.guildContext.guildError("Could not wipe channel:", err);
+            });
+        }
     }
   }
 
@@ -37,11 +46,11 @@ export class DevCommandController extends CommandController {
     // TODO: don't delete things we find in the current database.
 
     // Very rudementary auto-deletion of categories and their children.
-    for(let cacheMeta of this.guildContext.guild.channels.cache) {
+    for (let cacheMeta of this.guildContext.guild.channels.cache) {
       const channel = cacheMeta[1];
-      if((channel.name.endsWith("-chat") || channel.name.endsWith("-voice")) && channel.type === "category") {
+      if ((channel.name.endsWith("-chat") || channel.name.endsWith("-voice")) && channel.type === "category") {
         const category: Discord.CategoryChannel = <Discord.CategoryChannel>channel;
-        for(let childCacheMeta of category.children) {
+        for (let childCacheMeta of category.children) {
           const childChannel = childCacheMeta[1];
           await childChannel.delete("StudyBot dev reset command.");
           await DiscordUtils.rateLimitAvoidance();
@@ -53,18 +62,18 @@ export class DevCommandController extends CommandController {
     }
 
     // Roles
-    for(let roleMeta of this.guildContext.guild.roles.cache) {
+    for (let roleMeta of this.guildContext.guild.roles.cache) {
       const role = roleMeta[1];
-      if(role.name === "verified") {
+      if (role.name === "verified") {
         await role.delete("StudyBot dev reset command.");
         await DiscordUtils.rateLimitAvoidance();
         continue;
       }
 
-      if(role.name.endsWith("-ta")) {
+      if (role.name.endsWith("-ta")) {
         // Try to find matching non-ta role.
         const nonTaRole = this.guildContext.guild.roles.cache.find(r => r.name === role.name.substr(0, role.name.length - "-ta".length));
-        if(nonTaRole) {
+        if (nonTaRole) {
           await nonTaRole.delete("StudyBot dev reset command.");
           await DiscordUtils.rateLimitAvoidance();
         }
@@ -73,5 +82,33 @@ export class DevCommandController extends CommandController {
         await DiscordUtils.rateLimitAvoidance();
       }
     }
+  }
+
+  /**
+   * Deletes all messages in a text channel.
+   */
+  private async wipeTextChannel(channel: Discord.TextChannel): Promise<void> {
+    console.log("Dev wipechannel command started.");
+
+    let cacheSize = channel.messages.cache.size;
+    // Delete in groups of 100 until the messages are too old and must be deleted one at a time.
+    while (cacheSize > 0) {
+      const deletedSize = (await channel.bulkDelete(100, true)).size;
+      await DiscordUtils.rateLimitAvoidance(1500);
+      cacheSize = (await channel.messages.fetch()).size;
+      if(cacheSize > 0 && deletedSize < 100) {
+        // We couldn't delete a full 100 messages, which means that at this point the messages are too old
+        // and must be deleted one at a time.
+        while(cacheSize > 0) {
+          for(let message of channel.messages.cache) {
+            await message[1].delete({ reason: "StudyBot dev wipechannel command." });
+            await DiscordUtils.rateLimitAvoidance(900);
+          }
+          cacheSize = (await channel.messages.fetch()).size;
+        }
+      }
+    }
+
+    console.log("Dev wipechannel command complete.");
   }
 }
