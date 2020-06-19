fc9db4bc1fe357bb96c0a2e3f73d8918385cf0a7
Mitch Talmadge <mitch@mitchtalmadge.com>
2020-06-19 00:34:46 -0600

    Temporarily(?) disable rate limit avoidance.

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -7,6 +7,7 @@ export class DiscordUtils {
    * Rate limits happen at about 120 events per 60 seconds, or 2 per second.
    */
   public static async rateLimitAvoidance(delayMs = 750): Promise<void> {
+    delayMs = 0;
     await timer(delayMs).toPromise();
   }
 
