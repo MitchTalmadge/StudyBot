6c5f127aee204cade8b758a23619a88c5ad42f4d
Mitch Talmadge <mitch@mitchtalmadge.com>
2020-07-02 22:18:19 -0600

    Auto-apply verification role when trying to verify if somehow missing and user is verified.

--- src/controllers/channel/verification.ts
+++ src/controllers/channel/verification.ts
@@ -42,28 +42,44 @@ export class VerificationChannelController extends ChannelController {
       user = await UserDatabaseService.findOrCreateUser(message.author.id);
     } catch(err) {
       this.guildContext.guildError(`Could not retrieve user object from DB while checking verification status for ${DiscordUtils.describeUserForLogs(message.author)}`, err);
-      DiscordMessageUtils.sendTempMessage(VerificationChannelController.MESSAGE_DELAY, message.channel, `Sorry, ${message.author}, there was an error while checking your verification status. Please try again or ask an admin for help!`);
+      DiscordMessageUtils.sendTempMessage(
+        VerificationChannelController.MESSAGE_DELAY, 
+        message.channel, 
+        `Sorry, ${message.author}, there was an error while checking your verification status. Please try again or ask an admin for help!`);
       return;
     }
 
     if(user) {
       switch(user.verificationStatus) {
         case VerificationStatus.VERIFIED:
-          await DiscordMessageUtils.sendTempMessage(VerificationChannelController.MESSAGE_DELAY, message.channel, `${message.author}, you are already verified!`);
+          await DiscordMessageUtils.sendTempMessage(
+            VerificationChannelController.MESSAGE_DELAY, 
+            message.channel, 
+            `${message.author}, you are already verified!`);
+          await UserDatabaseService.setUserVerified(this.guildContext, message.member);
           return;
         case VerificationStatus.CODE_SENT:
           // Check if the user has input a code.
           if(contents.trim() === user.verificationCode) {
             // Code matches.
-            await DiscordMessageUtils.sendTempMessage(VerificationChannelController.MESSAGE_DELAY, message.channel, `Success! ${message.author}, you will be able to speak momentarily. Thanks for helping to keep the server safe!`);
+            await DiscordMessageUtils.sendTempMessage(
+              VerificationChannelController.MESSAGE_DELAY, 
+              message.channel, 
+              `Success! ${message.author}, you will be able to speak momentarily. Thanks for helping to keep the server safe!`);
             UserDatabaseService.setUserVerified(this.guildContext, message.member)
               .catch(err => {
                 console.error(`Could not set user with ID ${message.author.id} as verified`, err);
-                DiscordMessageUtils.sendTempMessage(VerificationChannelController.MESSAGE_DELAY, message.channel, `Oops! Sorry ${message.author}, there was an error while giving you the verified role. Please ask an admin for help!`);
+                DiscordMessageUtils.sendTempMessage(
+                  VerificationChannelController.MESSAGE_DELAY, 
+                  message.channel, 
+                  `Oops! Sorry ${message.author}, there was an error while giving you the verified role. Please ask an admin for help!`);
               });
           } else {
-            await DiscordMessageUtils.sendTempMessage(VerificationChannelController.MESSAGE_DELAY, message.channel, `Sorry ${message.author}, that code does not look correct. Try again, or ask an admin if you need a new email sent out.`);
-            // TODO: Allow user to try new student ID? (Typo)
+            await DiscordMessageUtils.sendTempMessage(
+              VerificationChannelController.MESSAGE_DELAY, 
+              message.channel, 
+              `Sorry ${message.author}, that code does not look correct. Try again, or ask an admin if you need a new email sent out.`);
+            // TODO: Allow user to try new student ID? (e.g. typo)
           }
           return;
         default:
